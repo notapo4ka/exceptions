8b0012fca3f297225e51490d2f4f68b3bef3c690
notapo4ka <112635391+notapo4ka@users.noreply.github.com>
2023-03-28 14:57:13 +0300

    Update ArrayValueCalculator.java

--- src/ArrayValueCalculator.java
+++ src/ArrayValueCalculator.java
@@ -1,33 +1,28 @@
 public class ArrayValueCalculator {
 
-    public static String doCalc(String stringArray[][]) throws ArraySizeException, ArrayDataException {
-        int sum = 0;
-
-        for (int i = 0; i < stringArray.length; i++) {
-            for (int j = 0; j < stringArray[i].length; j++) {
-                if (i <= 0 || j <= 0) {
-                    throw new IllegalArgumentException("Each element in the array must be non-negative and greater than zero");
-                }
-            }
-        }
-
-            if (stringArray == null) {
+    public static int doCalc(String[][] matrix) {
+            if (matrix == null) {
                 throw new NullPointerException("The array must not be null, the array must be 4x4");
             }
-            if (stringArray.length != 4 || stringArray[0].length != 4) {
+            if (matrix.length != 4) {
                 throw new ArraySizeException("The array must be 4x4");
             }
+            for (int i = 0; i < matrix.length; i++) {
+                if (matrix[i].length != 4) {
+                    throw new ArraySizeException("The array must be 4x4");
+                }
+            }
 
-            for (int i = 0; i < stringArray.length; i++) {
-                for (int j = 0; j < stringArray[i].length; j++) {
+            int sum = 0;
+            for (int i = 0; i < matrix.length; i++) {
+                for (int j = 0; j < matrix[i].length; j++) {
                     try {
-                        int number = Integer.parseInt(stringArray[i][j]);
-                        sum += number;
+                        sum += Integer.parseInt(matrix[i][j]);
                     } catch (NumberFormatException e) {
-                        throw new ArrayDataException("Невірні дані в комірці [" + i + "][" + j + "]: " + stringArray[i][j] + "\n" + "Дані повинні бути введені у форматі цифр або чисел '1', '2' та інше, але не 'abc', 'a', тощо");
+                        throw new ArrayDataException("Невірні дані в комірці [" + i + "][" + j + "]: " + matrix[i][j] + "\n" + "Дані повинні бути введені у форматі цифр або чисел '1', '2' та інше, але не 'abc', 'a', тощо");
                     }
                 }
             }
-            return "Сума чисел у масиві: " + sum;
+            return sum;
         }
     }
