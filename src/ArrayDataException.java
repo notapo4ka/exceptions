a8b8e9bb3de82a514966f85cbc0b2d4037b8b405
notapo4ka <112635391+notapo4ka@users.noreply.github.com>
2023-03-28 14:56:45 +0300

    Update ArrayDataException.java

--- src/ArrayDataException.java
+++ src/ArrayDataException.java
@@ -1,4 +1,4 @@
-public class ArrayDataException extends Exception {
+public class ArrayDataException extends RuntimeException {
     public ArrayDataException(String message) {
         super(message);
     }
