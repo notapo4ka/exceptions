81d41162230163d2af3f7d33338503c711a55f11
notapo4ka <112635391+notapo4ka@users.noreply.github.com>
2023-03-28 14:56:58 +0300

    Update ArraySizeException.java

--- src/ArraySizeException.java
+++ src/ArraySizeException.java
@@ -1,4 +1,4 @@
-public class ArraySizeException extends Exception {
+public class ArraySizeException extends RuntimeException {
     public ArraySizeException(String message) {
         super(message);
     }
