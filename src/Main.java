23c2caf97fc0241369baba30c217ef988a831b9d
notapo4ka <112635391+notapo4ka@users.noreply.github.com>
2023-03-28 14:57:24 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,13 @@
 public class Main {
 
     public static void main(String[] args) throws ArrayDataException, ArraySizeException {
-        String testArray[][] = {
+        String matrix[][] = {
                 {"ab","2", "3", "4"},
                 {"5", "6", "7", "8"},
                 {"9", "10", "11", "12"},
                 {"13", "14", "15", "16"}
         };
 
-        System.out.println(ArrayValueCalculator.doCalc(testArray));
+        System.out.println("Сума чисел у матриці: " + ArrayValueCalculator.doCalc(matrix));
     }
 }
